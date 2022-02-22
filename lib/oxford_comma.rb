5990b4db54b4211d9c95e625aa3d60e9843fde0c
Lynden Lim <lyndenlim15@gmail.com>
2022-02-22 10:55:58 -0500

    commit tree e85b805def112ad037f17a39bfb60b18c38244c7

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    array_result = array.map{|item| item}
+    if array.length > 2 
+        last_element = array_result.pop
+        array_result.push("and #{last_element}").join(', ')
+    elsif array.length == 2 
+        last_element = array_result.pop
+        array_result.push("and #{last_element}").join(' ')
+    elsif array.length == 1
+        array_result.join
+    end
 end
+
+print oxford_comma(["fish", 'head', "stink"])
